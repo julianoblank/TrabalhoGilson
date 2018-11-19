901fa217afb1097039ffec0b02a005d2f08d4ea9
juliano <julianoellwanger@gmail.com>
2018-11-19 20:04:20 -0200

    cadastrando novo login no webservice

--- app/src/main/java/com/example/juliano/trabalhogilson/MainActivity.java
+++ app/src/main/java/com/example/juliano/trabalhogilson/MainActivity.java
@@ -4,14 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usuario,senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usuario = findViewById(R.id.edUsuario);
+        senha = findViewById(R.id.edSenha);
 
 
     }
@@ -19,5 +30,32 @@ public class MainActivity extends AppCompatActivity {
     public void novoCadastro(View view){
         Intent novo = new Intent(this, NovoCadastro.class);
         startActivity(novo);
+    }
+
+    public void validaLogin(View view){
+        String url = "http://ghelfer-001-site8.itempurl.com/validaLogin.php";
+        RequestParams params = new RequestParams();
+        params.add("email",usuario.getText().toString());
+        params.add("senha",senha.getText().toString());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                try {
+                    String data = new String(response,"UTF-8");
+
+                    Toast.makeText(MainActivity.this, "logado", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] response, Throwable error) {
+
+            }
+        });
+
     }
 }
